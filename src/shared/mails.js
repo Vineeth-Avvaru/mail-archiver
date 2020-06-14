ae700c4c1d14195d4df07cfb2049011e048bbd6b
Vineeth Avvaru <avvaru95@gmail.com>
2020-06-14 20:06:03 +0900

    Dummy Data for mails added

--- src/shared/mails.js
+++ src/shared/mails.js
@@ -1,6 +1,6 @@
 const Mails = [
     {
-        from: "aaa2@example.com",
+        from: "aaa@example.com",
         to: ["bbb@example.com"],
         subject: "Lottery Time !! ",
         date: "06/12/2020 10:50",
@@ -8,11 +8,43 @@ const Mails = [
         attachment: true
     },
     {
-        from: "aaa1@example.com",
-        to: ["bbb@example.com","ccc@example.com","ddd@example.com"],
+        from: "jingping@example.com",
+        to: ["ggg@example.com","ccc@example.com","ddd@example.com"],
+        subject: "Travel Insurance ",
+        date: "06/05/2020 17:50",
+        body: "Taking Travel Insurance is always good for health and your loved ones",
+        attachment: false
+    },
+    {
+        from: "jonsmith@example.com",
+        to: ["babusmilz@example.com"],
+        subject: "Fun time at the circus",
+        date: "06/02/2020 19:50",
+        body: "Come and Have a good time at the new tokyo circus coming near you. see the attachment for details",
+        attachment: true
+    },
+    {
+        from: "careworld@example.com",
+        to: ["grandma@example.com","ccc@example.com","ddd@example.com"],
+        subject: "Time to retire",
+        date: "05/26/2020 13:14",
+        body: "Welcome to careworld!! The perfect paradise for retirement",
+        attachment: false
+    },
+    {
+        from: "chaku@example.com",
+        to: ["samaram@example.com"],
         subject: "This is to inform you guys that you are winner for the lucky draw",
         date: "05/12/2020 10:45",
         body: "Hurrayyy!! It's lucky time!!",
+        attachment: true
+    },
+    {
+        from: "bali@example.com",
+        to: ["platiz@example.com","ccc@example.com","ddd@example.com"],
+        subject: "Credit Card Offers",
+        date: "05/10/2020 01:45",
+        body: "Very Less time to apply of the new offers!",
         attachment: false
     }
 ];
